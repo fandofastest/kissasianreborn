d8e3e7e79556ed79488fe7fe9e2743c0833d8065
fando <fandofast@gmail.com>
2020-01-15 15:44:48 +0700

    Initial commit

---
+++ app/src/main/java/com/janjijiwa/kissasian/Config.java
@@ -0,0 +1,14 @@
+package com.janjijiwa.kissasian;
+
+public class Config {
+
+    // copy your api url from php admin dashboard & paste below
+    public static final String API_SERVER_URL = "https://janjijiwa.online/panel/api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "6gbi29eer6mehtqaa52j99c0";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "https://janjijiwa.online/panel/api/terms/";
+
+}
